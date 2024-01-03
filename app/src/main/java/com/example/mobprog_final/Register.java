c16c0ff0da037867c2167b415a83deda1b3fc8f9
chelluu <christabeldior@gmail.com>
2024-01-04 02:56:04 +0700

    allll my laiaiaiiaaiiif

---
+++ app/src/main/java/com/example/mobprog_final/Register.java
@@ -0,0 +1,19 @@
+package com.example.mobprog_final;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+        
+    }
+}
