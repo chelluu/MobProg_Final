af1ec531ae3e9531c1515380965780c5b63e6fff
Novita Aryanti <novitaaryanti25@gmail.com>
2024-01-04 21:09:55 +0700

    commit last

---
+++ app/src/main/java/com/example/moprog_final_1/NotifActivity.java
@@ -0,0 +1,53 @@
+package com.example.moprog_final_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class NotifActivity extends AppCompatActivity {
+
+    RecyclerView notif_recycler_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notif);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("Notifications");
+        actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_24);
+
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        NotifModel[] notif_data = new NotifModel[]{
+                new NotifModel("New article from NNC Indonesia", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", R.drawable.user),
+                new NotifModel("Popular article from #Finance", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", R.drawable.hashtag)
+        };
+
+        notif_recycler_view = findViewById(R.id.recycler_view_notif);
+
+        NotifAdapter notifAdapter = new NotifAdapter(notif_data);
+
+        notif_recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        notif_recycler_view.setAdapter(notifAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/moprog_final_1/NotifAdapter.java
@@ -0,0 +1,71 @@
+package com.example.moprog_final_1;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.View_holder> {
+
+    NotifModel[] notif_data_list;
+
+    public NotifAdapter(NotifModel[] notif_data_list) {
+        this.notif_data_list = notif_data_list;
+    }
+
+    @NonNull
+    @Override
+    public View_holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+        View notif_list_items = layoutInflater.inflate(R.layout.recycler_view_notif, null);
+
+        View_holder view_holder = new View_holder(notif_list_items);
+
+        return view_holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotifAdapter.View_holder holder, int position) {
+        NotifModel notif_model = notif_data_list[position];
+
+        holder.notif_img.setImageResource(notif_model.getImg());
+        holder.notif_title.setText(notif_model.getTitle());
+        holder.notif_text.setText(notif_model.getText());
+
+        holder.notif_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent clickNotifIntent = new Intent(view.getContext(), MainActivity.class);
+                view.getContext().startActivity(clickNotifIntent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return notif_data_list.length;
+    }
+
+    public class View_holder extends RecyclerView.ViewHolder {
+        ImageView notif_img;
+        TextView notif_title, notif_text;
+        CardView notif_card;
+
+        public View_holder(@NonNull View itemView) {
+            super(itemView);
+
+            notif_img = itemView.findViewById(R.id.imageNotifView);
+            notif_title = itemView.findViewById(R.id.titleNotifView);
+            notif_text = itemView.findViewById(R.id.textNotifView);
+            notif_card = itemView.findViewById(R.id.notifCard);
+        }
+    }
+}
+
